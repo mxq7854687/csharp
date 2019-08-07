04bfa709c4c48c574157ad360db10f2ccd00191c
richard <richardlyk1108@gmail.com>
2019-08-07 23:49:25 +0800

    cs game

---
+++ WarriorWars/WarriorWars/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using WarriorWars.Enum;
+using System;
+
+namespace WarriorWars
+{
+    public class EntryPoint
+    {
+        static Random rng = new Random();
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Bob", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Joe", Faction.BadGuy);
+
+            while(goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if(rng.Next(0,10)<5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+            }
+        }
+    }
+}
